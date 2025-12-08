3dd2db07d8b3e53ae618350d30f8da55817b6391
Yongjin <hi653@naver.com>
2025-12-08 17:42:06 +0900

    test

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     # 문구를 마음대로 바꿔봐!
-    return "Hello! 이것은 자동 배포된 웹사이트입니다! Test 수정!"
+    return "Hello! 이것은 자동 배포된 웹사이트입니다! Test 수정! 222"
 
 if __name__ == '__main__':
     # 서버가 제공하는 PORT 환경변수를 사용하고, 없으면 5000번을 쓴다.
