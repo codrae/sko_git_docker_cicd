dae01d550b7b9b44b8617783fb5af2234063da84
Yongjin <hi653@naver.com>
2025-12-08 17:22:10 +0900

    test : text 변경후 자동 배포 테스트

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello! CI/CD와 Docker 체험 성공입니다!"
+    return "Hello! CI/CD와 Docker 체험 성공입니다! 수정수정 이렇게 !"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
