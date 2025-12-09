d178e400149e29db24a5a7bc2219c592cf2ca193
Yongjin <hi653@naver.com>
2025-12-09 14:35:49 +0900

    feat : Implement test_health function

--- test_app.py
+++ test_app.py
@@ -6,3 +6,12 @@ def test_hello():
     response = client.get('/')
     assert response.status_code == 200
     assert b"Hello!" in response.data
+
+
+def test_health():
+    client = app.test_client()
+    response = client.get('/health')
+
+    # /health 주소로 찔렀을 때 200이 나오면 서버는 멀쩡한 것임
+    assert response.status_code == 200
+    assert response.json['status'] == 'ok'
